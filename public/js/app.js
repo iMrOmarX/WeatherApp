8b6debb7e1754277634cfd275b91b4a049f61e6e
omar <omar.aburish@gmail.com>
2020-02-08 10:31:29 +0200

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+
+
+console.log("Clinet Side js is loaded")
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const msg1 = document.querySelector("#msg_1")
+const msg2 = document.querySelector("#msg_2")
+
+
+
+
+weatherForm.addEventListener('submit' , (e) => {
+    e.preventDefault()
+ 
+    const location = search.value
+    
+    msg1.textContent = ""
+    msg2.textContent = "Loading !"
+    
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            msg1.textContent = data.error
+            msg2.textContent = ''
+        } else {
+            msg1.textContent = data.location
+            msg2.textContent = data.forecast
+        } 
+        
+    })
+    })
+    
+})
+
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude , longtitude , callback) => {
+    const url = 'https://api.darksky.net/forecast/b2bffeff4b316c51e43d5a4e12d6d228/'+ latitude + ',' + longtitude + '?units=si'
+    request({url, json: true} , (error , {body}) =>{
+
+        if(error) {
+            callback('Unable to connect to weather services' , undefined)
+        } else if (body.error) { 
+            callback("Unable to find the location", undefined)
+        } else {
+            const {temperature , precipProbability} = body.currently
+            const {summary} = body.daily.data[0]
+            callback(undefined , summary + ' It is currently ' + temperature + ' degrees out. There is a ' + precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
